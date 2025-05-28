f2727e48a00d2f523f4cd30ac0ed3ada6a5bdbb6
ruka <ruka_ootani@mail.toyota.co.jp>
2025-05-28 11:49:36 +0900

    add column

---
+++ server/db/data/migration/20250528024122_add_column_children.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    await knex.schema.alterTable('children', (table) => {
+        table.date("birthday").notNullable();
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async (knex) => {
+    await knex.schema.alterTable('children',(table) => {
+        table.dropColumns("birthday");
+    });
+  };
+  
