41efd7e177bb62b8ab2ba623da913410285edb86
ruka <ruka_ootani@mail.toyota.co.jp>
2025-05-29 17:47:45 +0900

    file make

---
+++ server/src/routes/logRputes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const {} = require('../controllers/logController');
+
+router.post('/parent', );
+router.post('/childstate',);
+
+module.exports= router;
