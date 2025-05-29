e2809928100b8047825ef185648531bf35d83875
naoto-nakamura-ac <naoto_nakamura_ac@mail.toyota.co.jp>
2025-05-29 18:01:32 +0900

    file make

--- server/src/controllers/contactController.js
+++ server/src/controllers/contactController.js
@@ -1,7 +1,14 @@
+const SuggestLog = require('../models/SuggestLog')
 
 
-
-const createLog = async (req,res) => {}
+const createLog = async (req,res) => {
+  try{
+    
+  }catch(error){
+    console.log('error: ', error);
+    return res.status(500).json({ message: 'Server Error' });
+  }
+}
 const getLog = async (req,res) => {}
 const contactGemini = async (req,res) => {}
 
---
+++ server/src/models/SuggestLog.js
@@ -0,0 +1,8 @@
+const db = require('../../db/index');
+
+const SUGGEST_LOG_TABLE = 'suggest_log';
+
+module.exports = {
+  
+
+}
