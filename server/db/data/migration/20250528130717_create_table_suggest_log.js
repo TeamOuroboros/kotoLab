84c9f1403a5881c2e6a9ae5682e221c1af8d7619
ruka <ruka_ootani@mail.toyota.co.jp>
2025-05-29 10:44:16 +0900

    migrate make

---
+++ server/db/data/migration/20250528130717_create_table_suggest_log.js
@@ -0,0 +1,15 @@
+exports.up = async (knex) => {
+    await knex.schema.createTable('suggest_log', (table) => {
+      table.increments('id').primary();
+      table.integer('log_id').notNullable();
+      table.foreign('log_id').references('log.id').onDelete('CASCADE');
+      table.string('log').notNullable();
+      table.string('role').notNullable();
+      table.timestamp('created_at', { useTz: true }).defaultTo(knex.fn.now());
+    });
+  };
+  
+  exports.down = async (knex) => {
+    await knex.schema.dropTable('suggest_log');
+  };
+  
