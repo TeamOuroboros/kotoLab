b268ba7569f0613fd8cf706b371e31ad81f6195f
ruka <ruka_ootani@mail.toyota.co.jp>
2025-05-29 22:27:46 +0900

    Co-authored-by: naoto-nakamura-ac <naoto-nakamura-ac@users.noreply.github.com>

--- server/src/models/Logs.js
+++ server/src/models/Logs.js
@@ -1,4 +1,19 @@
 const db = require('../../db/index');
+const LOG_TABLE = 'log';
+const LOG_CHILD_TABLE = 'log_child';
 
+module.exports = {
+	async insParenrLog(insertData) {
+		return await db(LOG_TABLE).insert(insertData);
+	},
 
-module.exports = {}
+	async findLogId(user_id, log_date) {
+		return await db(LOG_TABLE).select('id').where({ user_id, log_date }).first();
+	},
+
+	async insChildLog(insertData) {
+        console.log("🙅🏻",insertData);
+        
+		return await db(LOG_CHILD_TABLE).insert(insertData);
+	},
+};
