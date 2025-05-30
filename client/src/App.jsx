2f90b7ed6e699fc150f7137d53eeab653a388fd9
Hideaki-Umezawa <hideaki_umezawa@mail.toyota.co.jp>
2025-05-30 16:19:39 +0900

    イン画面途中

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,68 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React from "react";
+import { Box, Text, Button, Icon } from "@yamada-ui/react";
+import { MdWbSunny, MdNightsStay, MdSettings } from "react-icons/md";
 
 function App() {
-	const [count, setCount] = useState(0);
-	// async function userData() {
-	// 	const data = await fetch('/api').then((res) => res.json());
-	// 	console.log('🍓 ~ userData ~ data:', data);
-	// }
-	// userData();
+  return (
+    // ボタンcss共通
+    // 	<Button
+    // 	bg="#bcd4c1"
+    // 	minW={0}
+    // 	p={2}
+    // 	borderRadius="full"
+    // 	boxShadow="none"
+    // 	_hover={{ bg: "#a7c8b1" }}
+    //   >
+    <Box>
+      {/* 日時温度 ======================================================後でこれからAPIのデータ元に作成しまーす*/}
+      <Box textAlign="center" display="flex">
+        <Text fontWeight="bold" fontSize="20px" verticalAlign="middle">
+          5/10 ☀️
+        </Text>
+        <Text ml="auto">
+          <Icon as={MdWbSunny} verticalAlign="middle" fontSize="15px" /> 25°C /
+          {"  "}
+          <Icon as={MdNightsStay} verticalAlign="middle" fontSize="15px" /> 12°C
+        </Text>
+      </Box>
 
+      {/* きょうをつくろう。 のテキスト*/}
+      <Box alignItems="center" justifyContent="center" flexDirection="column">
+        <Text fontSize="30px" my={10}>
+          きょうをつくろう。
+        </Text>
 
-	return <></>;
+        {/* 提案ボタン　記録 */}
+        <Box display="flex" flexDirection="column" gap={30} rounded="full">
+          <Button
+            bg="#bcd4c1"
+            w="150"
+            h="50"
+            _hover={{ bg: "#a7c8b1" }}
+            rounded="full"
+          >
+            提案
+          </Button>
+          <Button
+            bg="#bcd4c1"
+            w="150"
+            h="50"
+            _hover={{ bg: "#a7c8b1" }}
+            rounded="full"
+          >
+            記録
+          </Button>
+        </Box>
+      </Box>
+
+      {/* 設定ボタン */}
+      <Box alignSelf="flex-end" align="right">
+        <Button bg="#bcd4c1" p={2} _hover={{ bg: "#a7c8b1" }} rounded="full">
+          <Icon as={MdSettings} />
+        </Button>
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import LoginForm from "./components/LoginForm.jsx";
-import RegisterForm from "./components/RegisterForm.jsx";
+// import LoginForm from "./components/LoginForm.jsx";
+// import RegisterForm from "./components/RegisterForm.jsx";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router";
 
 import "./index.css";
@@ -10,10 +10,10 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<LoginForm />} />
-      <Route path="/register" element={<RegisterForm />} />
-      <Route path="/records" element={<App />} />
+      {/* <Route path="/" element={<Navigate to="/login" />} /> */}
+      {/* <Route path="/login" element={<LoginForm />} /> */}
+      {/* <Route path="/register" element={<RegisterForm />} /> */}
+      <Route path="/" element={<App />} />
       {/* 必要時は以下に各自でRouteを追加 */}
     </Routes>
   </BrowserRouter>
