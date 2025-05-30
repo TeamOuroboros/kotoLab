eda1cbe042ad7c1164578373ca6cfd7364da02be
Tsuruta-Yuki <yuki_tsuruta_aa@mail.toyota.co.jp>
2025-05-30 11:47:14 +0900

    必要パッケージインストール

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import LoginForm from "./components/LoginForm.jsx";
+import RegisterForm from "./components/RegisterForm.jsx";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/register" element={<RegisterForm />} />
+      <Route path="/records" element={<App />} />
+      {/* 必要時は以下に各自でRouteを追加 */}
+    </Routes>
+  </BrowserRouter>
+);
